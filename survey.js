e79d64a8b4688cdad33985d226da51260e498891
Timi Alabi <alabitimi65@gmail.com>
2022-06-09 07:38:07 +0000

    creating a survey app using the readline function

---
+++ survey.js
@@ -0,0 +1,59 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What do you think of Node.js? ", (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
+rl.question("What's your name? Nicknames are aslo acceptable :) ", (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
+
+rl.question("What's an activity you like doing? ", (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
+
+rl.question("What do you listen to while doing that? ", (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
+
+rl.question(
+  "Which meal is your favorite? (eg: dinner, brunch, etc. )",
+  (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+
+    rl.close();
+  }
+);
+
+rl.question("What's your favourite thing to eat for that meal ", (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
+
+rl.question("Which sport is your absolute favourite? ", (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
+
+rl.question(
+  "What is your superpower? In a few words, tell us what you are ammzaing at! ",
+  (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+
+    rl.close();
+  }
+);
